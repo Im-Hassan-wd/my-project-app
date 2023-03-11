8bd6a2e9e91ab1bcbfe4ff1e4e8a942ba6505ee5
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-11 16:23:23 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link :to="{ name: 'Home' }">Home</router-link> |
+  <router-link :to="{ name: 'About' }">About</router-link> 
+  <router-view />
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+    }
+  },
+  methods: {},
+  created() {}
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+</style>
