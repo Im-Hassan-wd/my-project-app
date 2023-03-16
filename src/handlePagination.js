11b4dab1380a5659ef8c915a7b91d7124e7e5a05
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-16 19:31:11 +0100

    scroll to top when paginated

--- src/handlePagination.js
+++ src/handlePagination.js
@@ -7,6 +7,13 @@ export default function handlePagination(data) {
     return { index: item, value: `this_${item}` };
   });
 
+  const scrollTop = () => {
+    window.scrollTo({
+      behaviour: "smooth",
+      top: "0px"
+    })
+  }
+
   const perPage = 10;
 
   const paginatedData = computed(() =>
@@ -15,17 +22,20 @@ export default function handlePagination(data) {
 
   const nextPage = () => {
     if (page.value !== Math.ceil(data.value.length / perPage)) {
+      scrollTop()
       page.value += 1;
     }
   };
-
+  
   const backPage = () => {
     if (page.value !== 1) {
+      scrollTop()
       page.value -= 1;
     }
   };
-
+  
   const goToPage = (numPage) => {
+    scrollTop()
     page.value = numPage;
   };
 
